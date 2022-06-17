f0fa0e9e7e26b35f2a902ad74b9466f31bee1171
iamsurajitmishra <mishrasurajit69@gmail.com>
2022-06-17 20:27:22 +0530

    Mapper-util-deleted

--- src/main/java/com/sqs/mq/controller/SQSProducerController.java
+++ src/main/java/com/sqs/mq/controller/SQSProducerController.java
@@ -2,28 +2,36 @@ package com.sqs.mq.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.sqs.mq.model.Employe;
-import com.sqs.mq.util.MapperUtil;
 import io.awspring.cloud.messaging.core.QueueMessagingTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+
 @RestController
 @RequestMapping("/api/v1")
 public class SQSProducerController {
 
+    private ObjectMapper objectMapper;
+
     @Autowired
     private QueueMessagingTemplate queueMessagingTemplate;
+
+    @PostConstruct
+    public void init(){
+        objectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
+    }
 
     @Value("${cloud.aws.end-point.uri}")
     private String endPoint;
 
     @PostMapping("/put")
     public void putMessageToQ(@RequestBody Employe employe) throws JsonProcessingException {
-        queueMessagingTemplate.send(endPoint, MessageBuilder.withPayload(new MapperUtil(new ObjectMapper())
-                .convertPOJOToJSON(employe))
+        queueMessagingTemplate.send(endPoint, MessageBuilder.withPayload(objectMapper.writeValueAsString(employe))
                 .build());
     }
 
